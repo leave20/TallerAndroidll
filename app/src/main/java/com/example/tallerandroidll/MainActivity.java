3ce6b23a15fcb4eb3651e9badb686d62c0918755
leave20 <thetai6102@gmail.com>
2020-11-09 17:05:12 -0500

    adding permissions

--- app/src/main/java/com/example/tallerandroidll/MainActivity.java
+++ app/src/main/java/com/example/tallerandroidll/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.tallerandroidll;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+private static final int READ_CONTACTS_PERMISSIONS=100;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int permissionCheck= ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS);
+        if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.READ_CONTACTS)) {
+            }else{
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_CONTACTS},READ_CONTACTS_PERMISSIONS);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case READ_CONTACTS_PERMISSIONS:{
+                if (grantResults.length>0 &&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MainActivity.this, "Permisos OK",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Sin permisos ",Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+
     }
 }
